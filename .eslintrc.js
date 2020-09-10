7c34a35f1d0f481432aaa0775364f485a75f18a8
Zhalgas Azhibayev <zhiga_@mail.ru>
2020-09-11 01:14:46 +0600

    set eslint rules with npm scripts for linting on windows. Use lintProdWin before commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,10 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
+    'no-undef': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
+    'quotes': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
+    'comma-dangle': process.env.NODE_ENV === 'development' ? ['warn', 'always-multiline'] : ['error', 'always-multiline'],
   }
 }
