fb26677bca38d6670a692a15a067e52256335c37
Zhalgas Azhibayev <zhiga_@mail.ru>
2020-09-11 14:06:42 +0600

    app layout

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template lang="pug">
-  router-view
+  component(:is="layout + '-layout'")
+    router-view.flex-stretch.flex-column
 </template>
+
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  name: 'app',
+  components: {
+    'default-layout': () => import('@/layouts/default'),
+  },
+  computed: {
+    ...mapGetters('layout', ['layout']),
+  },
+}
+</script>
 
 <style lang="sass">
 @import "style/global"
---
+++ src/layouts/default.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+.layout-default.flex-column
+  .main-container
+    slot
+</template>
+
+<script>
+export default {
+  name: 'layout-default',
+}
+</script>
+
+<style lang="sass" scoped>
+html,
+body
+  height: 100%
+.layout-default
+  max-width: 1200px
+  margin: 0 auto
+  padding: 60px 20px
+  @media (max-width: $to-sm)
+    padding: 20px
+</style>
